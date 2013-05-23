c3d4ed8f2b037c4ca685462c42c5eeb3203bbab2
Thomas Welton <thomas@clicklabs.co.uk>
2013-05-23 12:31:41 +0100

    Yeah turns out this was correct before and Nick wasn't decoding the URL before testing… :/

--- facebook.php
+++ facebook.php
@@ -85,7 +85,7 @@ class CI_Facebook extends Facebook {
 		$html = '';
 		foreach(self::$ogTags as $key => $value){
 			if(trim($value) != ''){
-				$html .= '<meta property="'.$key.'" content="'.addslashes($value).'">'."\n\t";
+				$html .= '<meta property="'.$key.'" content="'.htmlentities($value).'">'."\n\t";
 			}
 		}
 		return $html;
